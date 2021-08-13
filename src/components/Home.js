a2938f80464fe4aaba62ce243c4edbad800bf4db
mansoorameen <manzooravail@gmail.com>
2021-08-13 10:39:08 +0530

    added map

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
+import ReactMapGL, { NavigationControl} from "react-map-gl";
 
 const Home = () => {
-    return (
-        <div>
-            WELCOME TO DASHBOARD SETUP
-        </div>
-    )
-}
 
+    const [ viewport, setViewport] = useState({
+        // latitude: 45.4211,
+        // longitude: -75.6903,
+        latitude: 24.979940,
+        longitude: 55.118881,
+        width: '85%',
+        height: '100vh',
+        zoom: 12,
+        // pitch: 50
+      })
+
+      return (
+        <div className="home" >
+          <ReactMapGL {...viewport} 
+          // mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+          mapboxApiAccessToken="pk.eyJ1IjoibWFuc29vcmFtZWVuIiwiYSI6ImNrczh3Y2d6bTFyMTYybnBobzd4ZXUycXcifQ.Ebyu9N4yE8hppLp6JQmsjg"
+          mapStyle="mapbox://styles/mansoorameen/cks76xung60ih18kcuvver6yk"
+          onViewportChange={newViewport => {
+            setViewport(newViewport)
+          }}
+          >
+                {/* <NavigationControl /> */}
+    
+          </ReactMapGL>
+        </div>
+      );
+    }
 export default Home
